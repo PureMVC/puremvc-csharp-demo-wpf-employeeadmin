dea3aacf864e963aa1f398c2cdc0f1bebad9b27a
Andy Adamczak <andy.adamczak@puremvc.org>
2009-02-21 23:21:12 +0000

    Updated the copyright information.

--- EmployeeAdmin/View/RolePanelMediator.cs
+++ EmployeeAdmin/View/RolePanelMediator.cs
@@ -1,7 +1,7 @@
 ﻿/* 
- PureMVC C# Port by Andy Adamczak <andy.adamczak@puremvc.org>, et al.
- PureMVC - Copyright(c) 2006-08 Futurescale, Inc., Some rights reserved. 
- Your reuse is governed by the Creative Commons Attribution 3.0 License 
+	PureMVC CSharp / WPF / EmployeeAdmin Demo - Login
+	By Andy Adamczak <andy.adamczak@puremvc.org>
+	Copyright(c) 2009 Andy Adamczak, Some rights reserved.
 */
 
 using System;
